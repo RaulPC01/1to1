1131e4c579eaa57b5e530df54e1a5aa57781aacd
RaulPC01 <raulpereira-01@hotmail.com>
2024-04-04 17:06:33 +0200

    commit

---
+++ angular/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User{
+    dni: string;
+    nombre: string;
+    apellidos: string;
+    email: string;
+    contrasenya: string;
+    telefono: string;
+    ubicacion: string;
+    data: Date;
+}
