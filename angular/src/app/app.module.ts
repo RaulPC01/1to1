9b640876a6e5c45de05866ec930c114d2dc47cd7
RaulPC01 <raulpereira-01@hotmail.com>
2024-04-04 16:55:24 +0200

    commit

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { CreacionServicioComponent } from './creacion-servicio/creacion-servicio.component';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderHomeComponent } from './header-home/header-home.component';
+import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { HomeComponent } from './home/home.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ServicesComponent } from './services/services.component';
+import { ServicioComponent } from './servicio/servicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    CreacionServicioComponent,
+    ErrorComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeaderHomeComponent,
+    LoginComponent,
+    PerfilComponent,
+    HomeComponent,
+    RegistroComponent,
+    ServicesComponent,
+    ServicioComponent
   ],
   imports: [
     BrowserModule,
