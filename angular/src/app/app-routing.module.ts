19d8c890eaa8ef5765c07aab3d2a231f2a7ac812
ArianLanuza <arianlaro@gmail.com>
2024-04-05 16:15:42 +0200

    c

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { ErrorComponent } from './components/error/error.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { MainCompradorComponent } from './components/logged/comprador/main-comprador/main-comprador.component';
+import { ServicioComponent } from './components/logged/comprador/servicio/servicio.component';
+import { ServiciosComponent } from './components/logged/comprador/servicios/servicios.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   
   { path: 'contacto', component: ContactoComponent},
+
+  { path: 'home-comprador', component: MainCompradorComponent},
+  { path: 'servicio', component: ServicioComponent},
+  { path: 'servicios', component: ServiciosComponent},
+
   // =====================================================
   {
     path: 'home',
--- angular/src/app/components/logged/comprador/main-comprador/main-comprador.component.ts
+++ angular/src/app/components/logged/comprador/main-comprador/main-comprador.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-comprador',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-comprador.component.css']
 })
 export class MainCompradorComponent {
+  topRatedServices: any[] = [];
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getTopRatedServices();
+  }
+
+  nEstrellas(score: number): any[] {
+    return Array(score).fill(0);
+  }
+
+  public getTopRatedServices(): void {
+    const baseUrl = 'http://localhost:8000/api/services/top-valorated';
+  
+    this.http.post<any[]>(baseUrl, {}).subscribe(
+      (data: any[]) => {
+        this.topRatedServices = data;
+      },
+      (error) => {
+        console.log('Error al obtener los servicios mejor valorados: ', error);
+      }
+    );
+  }
+
+  navegarDetalleServicio(servicioId: number) {
+    this.router.navigate(['/servicios', servicioId]); // Navegar al detalle del servicio al hacer clic
+  }
 }
