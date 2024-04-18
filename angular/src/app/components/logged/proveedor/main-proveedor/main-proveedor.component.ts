b5005af4dd4ad5ed6e98d6f518fc169f206821e5
ArianLanuza <arianlaro@gmail.com>
2024-04-18 17:24:27 +0200

    c

--- angular/src/app/components/logged/proveedor/main-proveedor/main-proveedor.component.ts
+++ angular/src/app/components/logged/proveedor/main-proveedor/main-proveedor.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-proveedor',
   templateUrl: './main-proveedor.component.html',
   styleUrls: ['./main-proveedor.component.css']
 })
-export class MainProveedorComponent {
+export class MainProveedorComponent implements OnInit {
+  nuevoServicio = { nombre: '' };
+  servicios = [{ nombre: 'Ejemplo de Servicio' }];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  crearServicio() {
+    this.servicios.push({...this.nuevoServicio});
+    this.nuevoServicio.nombre = ''; // Reset the form
+  }
+
+  editarServicio(servicio: any) {
+    // Agregar la lógica de edición aquí
+  }
+
+  eliminarServicio(servicio: any) {
+    const index = this.servicios.indexOf(servicio);
+    if (index > -1) {
+      this.servicios.splice(index, 1);
+    }
+  }
 }
