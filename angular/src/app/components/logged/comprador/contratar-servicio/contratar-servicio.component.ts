7a3820a7b5f86106682de9825e76b1a66dd4e691
RaulPC01 <raulpereira-01@hotmail.com>
2024-05-08 13:35:03 +0200

    commit

--- angular/src/app/components/logged/comprador/contratar-servicio/contratar-servicio.component.ts
+++ angular/src/app/components/logged/comprador/contratar-servicio/contratar-servicio.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/user.service';
 import { ServicioService } from 'src/app/servicio.service';
+
+// Función validadora para asegurar que una fecha esté dentro de un rango especificado.
+function dateRangeValidator(minDate: Date, maxDate: Date): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const date = new Date(control.value);
+    if (!control.value || date < minDate || date > maxDate) {
+      return { 'dateRange': true };
+    }
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-contratar-servicio',
@@ -21,8 +32,11 @@ export class ContratarServicioComponent implements OnInit {
   nombreLength: number = 0;
   descripcionLength: number = 0;
   servicio: any;
-
   acceptado: boolean = false;
+
+  minDate: string | undefined;
+  maxDate: string | undefined;
+
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
@@ -30,145 +44,122 @@ export class ContratarServicioComponent implements OnInit {
     private route: ActivatedRoute,
     private userService: UserService,
     private servicioService: ServicioService
-  ) {
-    
-  }
+  ) {}
 
   ngOnInit(): void {
+    const today = new Date();
+    const nextYear = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+    this.minDate = today.toISOString().split('T')[0];
+    this.maxDate = nextYear.toISOString().split('T')[0];
+
     this.route.paramMap.subscribe(params => {
       const servicioId = params.get('id_servicio');
-      console.log('ID del servicio:', servicioId);
-  
-      // Inicializar el formulario FormGroup
+
+      // Inicialización del formulario con validaciones y campos requeridos.
       this.servicioForm = this.formBuilder.group({
-        id_user: ['', Validators.required], // Cambiado a 'id_user'
-        id_servicio: [servicioId, Validators.required], // Asignar el servicioId obtenido de la URL
+        id_user: ['', Validators.required],
+        id_servicio: [servicioId, Validators.required],
         id_user_proveedor: ['', Validators.required],
         nombre_Servicio: ['', Validators.required],
         name_user_solicitud: ['', Validators.required],
         descripcion: ['', Validators.required],
-        date_servicio: ['', Validators.required],
+        date_servicio: ['', [Validators.required, dateRangeValidator(today, nextYear)]],
         telefono_user: ['', Validators.required],
-        accepted: [this.acceptado], // Establecer accepted en false por defecto
+        accepted: [this.acceptado],
       });
-  
-      // Obtener los datos del perfil del usuario al iniciar el componente
+
       const token = localStorage.getItem('Idtoken');
-      if (token && servicioId !== null) {
+      if (token && servicioId) {
         this.obtenerPerfilUsuario(token, servicioId);
-        this.obtenerDetalleServicio(servicioId); // Pasar servicioId aquí
+        this.obtenerDetalleServicio(servicioId);
       } else {
-        console.error('El token no está definido en el almacenamiento local o servicioId es null');
-        // Manejar el error adecuadamente, por ejemplo, redirigir al usuario a la página de inicio de sesión
+        console.error('El token o el ID del servicio no está disponible.');
       }
     });
   }
-  
+
+  // Función para obtener detalles de un servicio específico mediante su ID.
   obtenerDetalleServicio(id_servicios: string): void {
     this.servicioService.obtenerDetalleServicio(id_servicios).subscribe(
-      (data) => {   
+      (data) => {
         console.log('JSON retornado por la API:', data);
-        
         this.servicio = data;
-        const nomServicio = this.servicio.tipo_servicio;
         if (this.servicio && this.servicio.id_servicios) {
           this.servicioForm.patchValue({
-            nombre_Servicio: nomServicio,
+            nombre_Servicio: this.servicio.tipo_servicio
           });
         } else {
           console.error('No se pudo obtener el id del servicio correctamente');
         }
-
       },
       (error) => {
         console.error('Error al obtener los detalles del servicio: ', error);
-        
       }
     );
   }
 
+  // Función para obtener el perfil del usuario actualmente logueado.
   obtenerPerfilUsuario(token: string, servicioId: string): void {
     this.userService.obtenerPerfilUsuario(token).subscribe(
       (data) => {
         console.log('Datos del usuario loggeado:', data);
         this.user = data;
-  
-        // Convertir el dni a un entero y establecerlo en el formulario
         const userId = this.user.dni;
         const telefono = this.user.phone;
         const nombreUser = this.user.name;
-        
-        console.log('Valor de userId:', userId);
         this.servicioForm.patchValue({
           id_user: userId,
           telefono_user: telefono,
-          name_user_solicitud: nombreUser,
+          name_user_solicitud: nombreUser
         });
-  
-        // Pasar servicioId a getUserByServiceId
-        if (servicioId) {
-          this.getUserByServiceId(servicioId); // Corregir aquí
-        } else {
-          console.error('El servicioId no está definido');
-        }
+        this.getUserByServiceId(servicioId);
       },
       (error) => {
         console.error('Error al obtener el perfil del usuario:', error);
       }
     );
   }
-  
+
+  // Función para obtener el usuario proveedor del servicio.
   getUserByServiceId(servicioId: string): void {
-    // Construir la URL de la solicitud
     this.userService.getUserByServiceId(servicioId).subscribe(
       (data) => {
-        // Manejar la respuesta exitosa
         console.log('Datos del usuario del servicio:', data);
-  
-        // Utilizar el ID del usuario proveedor del servicio
-        const userProvId = data.dni.toString(); // Convertir a cadena de texto
-  
+        this.userProv = data;
         this.servicioForm.patchValue({
-          id_user_proveedor: userProvId, // Asignar el ID del usuario proveedor al campo id_user_proveedor
+          id_user_proveedor: this.userProv.dni.toString()
         });
-        // Aquí puedes implementar la lógica para manejar los datos del usuario
       },
       (error: HttpErrorResponse) => {
-        // Manejar el error
         console.error('Error al obtener el usuario:', error);
-        // Aquí puedes implementar la lógica para manejar el error, como mostrar un mensaje al usuario
       }
     );
   }
-  
-  submitForm() {
 
-    // Console log de los datos del formulario antes de enviarlo al backend
+  // Función para enviar el formulario y procesar la solicitud del servicio.
+  submitForm() {
     console.log('Datos del formulario:', this.servicioForm.value);
-  
-    // Enviar los datos del formulario al backend
-    this.servicioService.enviarSolicitud(this.servicioForm.value)
-      .subscribe(
-        (data) => {
-          // Manejar la respuesta del backend si es necesario
+    if (this.servicioForm.valid) {
+      this.servicioService.enviarSolicitud(this.servicioForm.value).subscribe(
+        data => {
           console.log('Respuesta del servidor:', data);
-          this.showConfirmation = true; // Mostrar el mensaje de confirmación
+          this.showConfirmation = true;
           setTimeout(() => {
-            this.showConfirmation = false; // Ocultar el mensaje de confirmación después de 4 segundos
+            this.showConfirmation = false;
             this.router.navigate(['/home-comprador']);
-          }, 4000); // Tiempo en milisegundos (en este caso, 4 segundos)
-          this.servicioForm.reset(); // Restablecer los valores del formulario
-          // Redirigir al usuario a otra página, mostrar mensaje de éxito, etc.
+          }, 4000);
+          this.servicioForm.reset();
         },
         (error: HttpErrorResponse) => {
           console.error('Error al enviar el formulario:', error);
-          // Manejar errores en caso de que la solicitud falle
-          // Redirigir al usuario a la página de inicio o mostrar un mensaje de error
-          this.router.navigate(['/']);
         }
       );
+    } else {
+      console.error('El formulario no es válido');
+    }
   }
 
+  // Métodos para gestionar la visualización de diferentes secciones del formulario.
   verDescripcion() {
     this.mostrarDescripcion = true;
     this.mostrarDate = false;
@@ -183,18 +174,20 @@ export class ContratarServicioComponent implements OnInit {
     this.mostrarDate = false;
     this.mostrarDescripcion = true;
   }
+
   verMetodoPago() {
     this.mostrarDescripcion = false;
     this.mostrarDate = false;
     this.mostrarMetodoPago = true;
-}
+  }
 
-anteriorDate() {
+  anteriorDate() {
     this.mostrarDescripcion = true;
     this.mostrarDate = false;
     this.mostrarMetodoPago = false;
-}
+  }
 
+  // Función para controlar la longitud de la descripción ingresada por el usuario.
   countDescripcionLength(event: any): void {
     const input = event.target as HTMLTextAreaElement;
     if (input.value.length > 200) {
