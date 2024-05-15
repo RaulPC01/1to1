31bbda182028cfcf084efb4ba72544aac8308f9d
Orteeuu <rorteub02@gmail.com>
2024-05-15 17:57:41 +0200

    ggg

---
+++ angular/src/app/politica-cokkies/politica-cokkies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-cokkies',
+  templateUrl: './politica-cokkies.component.html',
+  styleUrls: ['./politica-cokkies.component.css']
+})
+export class PoliticaCokkiesComponent {
+
+}
